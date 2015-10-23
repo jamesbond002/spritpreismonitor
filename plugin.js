788b4efd25ecd0d68786b426562d0cde48a90c01
Fred <jamesbond00700@yandex.com>
2015-10-23 15:41:43 +0200

    Create plugin.js

---
+++ plugin.js
@@ -0,0 +1,14 @@
+{
+"moduleId" : "jamesbond002/spritpreismonitor",
+"title" : "Spritpreismonitor",
+"subtitle" : "www.spritpreismonitor.de",
+"backButton" : true,
+"enableGPS" : true,
+"zoomControl" : false,
+"screenLockRot" : false,
+"reload" : false,
+"shouldOverrideUrlLoading1" : "http://www.spritpreismonitor.de",
+"shouldOverrideUrlLoading2" : "https://www.spritpreismonitor.de/",
+"loadUrl" : "http%3A%2F%2Fwww.spritpreismonitor.de%2Fsuche%2F%3Ftx_spritpreismonitor_pi1%5BsearchRequest%5D%5BplzOrtGeo%5D%3D#lat#%2C#lng#%26tx_spritpreismonitor_pi1%5BsearchRequest%5D%5Bumkreis%5D%3D5%26tx_spritpreismonitor_pi1%5BsearchRequest%5D%5BplzOrtGeo2%5D%3D%26tx_spritpreismonitor_pi1%5BsearchRequest%5D%5Bmax_tankstellen%5D%3D25%26tx_spritpreismonitor_pi1%5BsearchRequest%5D%5Bkraftstoffart%5D%3De5%26tx_spritpreismonitor_pi1%5BsearchRequest%5D%5Btankstellenbetreiber%5D%3D",
+"browserLaunchLink" : "http%3A%2F%2Fwww.spritpreismonitor.de%2Fsuche%2F%3Ftx_spritpreismonitor_pi1%5BsearchRequest%5D%5BplzOrtGeo%5D%3D#lat#%2C#lng#%26tx_spritpreismonitor_pi1%5BsearchRequest%5D%5Bumkreis%5D%3D5%26tx_spritpreismonitor_pi1%5BsearchRequest%5D%5BplzOrtGeo2%5D%3D%26tx_spritpreismonitor_pi1%5BsearchRequest%5D%5Bmax_tankstellen%5D%3D25%26tx_spritpreismonitor_pi1%5BsearchRequest%5D%5Bkraftstoffart%5D%3De5%26tx_spritpreismonitor_pi1%5BsearchRequest%5D%5Btankstellenbetreiber%5D%3D"
+}
